3955b027138e121e6a75ae3a08ceea048964e37e
Ming Fee <118496518+MingPunyaGit@users.noreply.github.com>
2023-09-25 16:15:41 +0700

    Update 71220960-03.py

--- 71220960-03.py
+++ 71220960-03.py
@@ -79,3 +79,80 @@ with open('data.json', 'w') as file:
 
 print('Data berhasil ditambahkan.')
 
+
+class Mahasiswa:
+
+    def __init__(self, nama, nim, prodi):
+        self.nama = nama
+        self.nim = nim
+        self.prodi = prodi
+        self.ips = 0
+
+    def get_nama(self):
+        return self.nama
+
+    def set_nama(self, nama):
+        self.nama = nama
+
+    def get_nim(self):
+        return self.nim
+
+    def set_nim(self, nim):
+        self.nim = nim
+
+    def get_prodi(self):
+        return self.prodi
+
+    def set_prodi(self, prodi):
+        self.prodi = prodi
+
+    def get_ips(self):
+        return self.ips
+
+    def set_ips(self, ips):
+        self.ips = ips
+
+    def print_informasi(self):
+        print("=== INFORMASI MAHASISWA ===")
+        print("Nama: " + self.nama)
+        print("NIM: " + self.nim)
+        print("Prodi: " + self.prodi)
+
+    def hitung_ips(self):
+        total_nilai = 0
+        total_sks = 0
+
+        jumlah_matkul = int(input("Masukkan jumlah mata kuliah: "))
+        for i in range(jumlah_matkul):
+            nama_matkul = input("Masukkan nama mata kuliah: ")
+            nilai_matkul = input("Masukkan nilai mata kuliah: ")
+            sks = int(input("Masukkan SKS mata kuliah: "))
+
+            nilai_angka = {
+                "A": 4,
+                "A-": 3.7,
+                "B+": 3.3,
+                "B": 3,
+                "B-": 2.7,
+                "C+": 2.3,
+                "C": 2,
+                "D": 1,
+                "E": 0,
+            }
+
+            total_nilai += nilai_angka[nilai_matkul] * sks
+            total_sks += sks
+
+        self.ips = round(total_nilai / total_sks, 2)
+
+
+if __name__ == "__main__":
+    mhs = Mahasiswa("Budi", "71210000", "FTI")
+
+    mhs.print_informasi()
+
+    mhs.hitung_ips()
+
+    print("IPS:", mhs.get_ips())
+    
+
