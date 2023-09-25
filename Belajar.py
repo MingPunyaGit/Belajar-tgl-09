a1c1507609e4b30d2a2afd5edd6d75e08594b73f
Ming Fee <118496518+MingPunyaGit@users.noreply.github.com>
2023-09-25 16:51:22 +0700

    Update Belajar.py

--- Belajar.py
+++ Belajar.py
@@ -52,4 +52,70 @@ if operasi in ('1', '2', '3', '4'):
             print('Tidak bisa membagi dengan 0')
 else:
     print('Pilihan operasi tidak valid')
+
+
+class Mahasiswa:
+    def __init__(self, nama, nim, prodi):
+        self.nama = nama
+        self.nim = nim
+        self.prodi = prodi
+        self.ips = 0
+
+    def get_nama(self):
+        return self.nama
+
+    def get_nim(self):
+        return self.nim
+
+    def get_prodi(self):
+        return self.prodi
+
+    def get_ips(self):
+        return self.ips
+
+    def print_informasi(self):
+        print("=== INFORMASI MAHASISWA ===")
+        print("Nama:", self.nama)
+        print("NIM:", self.nim)
+        print("Prodi:", self.prodi)
+        print("IPS:", format(self.ips, ".2f"))
+
+    def hitung_ips(self):
+        total_nilai = 0
+        total_sks = 0
+
+        jumlah_matkul = int(input("Masukkan jumlah mata kuliah: "))
+        for i in range(jumlah_matkul):
+            nama_matkul = input("Masukkan nama mata kuliah: ")
+            nilai_matkul = input("Masukkan nilai mata kuliah (A, A-, B+, dst.): ")
+            sks = int(input("Masukkan SKS mata kuliah: "))
+
+            nilai_angka = {
+                "A": 4,
+                "A-": 3.7,
+                "B+": 3.3,
+                "B": 3,
+                "B-": 2.7,
+                "C+": 2.3,
+                "C": 2,
+                "D": 1,
+                "E": 0,
+            }
+
+            total_nilai += nilai_angka.get(nilai_matkul.upper(), 0) * sks
+            total_sks += sks
+
+        if total_sks > 0:
+            self.ips = total_nilai / total_sks
+
+
+# Contoh penggunaan kelas Mahasiswa
+nama = input("Masukkan nama mahasiswa: ")
+nim = input("Masukkan NIM mahasiswa: ")
+prodi = input("Masukkan prodi mahasiswa: ")
+
+mahasiswa = Mahasiswa(nama, nim, prodi)
+mahasiswa.hitung_ips()
+mahasiswa.print_informasi()
+
   
